98a8b7cea185096e70307b3bdb99a7159928a45a
Jo Watson <jo.watson@northcoders.com>
2024-10-11 11:55:41 +0100

    Added node executable file for project

---
+++ run-project-generator-promises.js
@@ -0,0 +1,16 @@
+const { input } = require("@inquirer/prompts");
+const makeProjectWithPromises = require("./src/make-project-with-promises");
+
+const queryFilePath = {
+  type: "input",
+  name: "filePath",
+  message: "What is the path of your new project?",
+  default: "/",
+};
+
+async function runProjectGenerator() {
+  input(queryFilePath).then((answer) => {
+    makeProjectWithPromises(answer);
+  });
+}
+runProjectGenerator();
