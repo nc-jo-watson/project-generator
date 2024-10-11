f7f250e79a6b5a8c67aefc2b5f78b28894f57da2
Jo Watson <jo.watson@northcoders.com>
2024-10-11 09:57:48 +0100

    Fix: updated depreciated fs.rmdir to fs.rm

--- __tests__/utils.js
+++ __tests__/utils.js
@@ -2,7 +2,7 @@ const fs = require("fs/promises");
 
 const removeExistingProject = async () => {
   await fs
-    .rmdir(`./${testProjectName}`, { recursive: true, force: true })
+    .rm(`./${testProjectName}`, { recursive: true, force: true })
     .catch(() => {});
 };
 
